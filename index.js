43605f2a33fbe784e873755197e79cda59416d37
MinjiJo <MinjiJo@users.noreply.github.com>
2019-06-20 23:25:06 +0900

    practice middlewears

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 const app = express();
 
 const PORT = 4000;
@@ -9,6 +13,12 @@ const handleListening = () =>
 const handleHome = (req, res) => res.send("Hello from home");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(helmet());
 
 app.get("/", handleHome);
 
