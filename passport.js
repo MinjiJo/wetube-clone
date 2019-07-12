5dd63875150ba9895542b0a9e94f23b780c2b261
MinjiJo <MinjiJo@users.noreply.github.com>
2019-07-12 20:58:20 +0900

    Installing passport github and starting set up

--- passport.js
+++ passport.js
@@ -1,7 +1,17 @@
 import passport from "passport";
+import GithubStrategy from "passport-local-mongoose";
 import User from "./models/User";
+import { githubLoginCallback } from "./controllers/userController";
 
 passport.use(User.createStrategy());
 
+passport.use(
+  new GithubStrategy({
+    clientID: process.env.GH_ID,
+    clientSecret: process.env.GH_SECRET,
+    callbackURL: "http://localhost:4000/auth/github/callback"
+  }),
+  githubLoginCallback
+);
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
